da8738359e1e6508b3b0e92ee45499d5b661aead
Ever <everchan611@gmail.com>
2026-04-20 21:00:37 +0800

    feat: github actions deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# simple-mini-main
+# simple-qiankun
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/simple-qiankun/' : '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   server: {
     proxy: {
